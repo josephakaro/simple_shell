1691a9f9f655b141994c323076bb9d9d747503d9
josephmarko <josephmmakaro@gmail.com>
2023-08-23 19:58:29 +0000

    Resolved 40 line in function

--- shell.c
+++ shell.c
@@ -11,42 +11,34 @@
 
 int main(int argc, char **argv)
 {
-	char *buf = NULL, *delim = " \n";
+	char *buffer = NULL, *delim = " \n";
 	ssize_t line = 0;
 	size_t nread = 0;
 	int status = 0;
 	(void)argc;
 
-
-
 	do {
-
 		if (isatty(STDIN_FILENO))
 			printf(":) ");
+		line = getline(&buffer, &nread, stdin);
 
-		line = getline(&buf, &nread, stdin);
-
-		if (line == -1 || hsh_strcmp("exit\n", buf) == 0)
+		if (line == -1 || hsh_strcmp("exit\n", buffer) == 0)
 		{
-			free(buf);
+			free(buffer);
 			break;
 		}
-		buf[line - 1] = '\0';
-
-		if (hsh_strcmp("env", buf) == 0)
+		buffer[line - 1] = '\0';
+		if (hsh_strcmp("env", buffer) == 0)
 		{
 			hsh_env();
 			continue;
 		}
-
-		if (hsh_line(buf) == 1)
+		if (hsh_line(buffer) == 1)
 		{
 			status = 0;
 			continue;
 		}
-
-		argv = hsh_parse(buf, delim);
-
+		argv = hsh_parse(buffer, delim);
 		argv[0] = hsh_path(argv[0]);
 
 		if (argv[0] != NULL)
@@ -54,9 +46,7 @@ int main(int argc, char **argv)
 		else
 			perror("Error");
 		free(argv);
-
 	} while (1);
-
 	return (status);
 }
 
